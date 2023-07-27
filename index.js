4a46b4a4408947330b54af151b4df659bd46f7ce
Sam Scheding <sscheding@atlassian.com>
2023-07-27 23:31:12 +1000

    Add psuedoelements and specialcharacters

--- index.js
+++ index.js
@@ -1,2 +1,4 @@
 export { default as pseudoclasses } from './pseudoclasses'
+export { default as pseudoelements } from './pseudoelements'
 export { default as declarations } from './declarations'
+export { default as specialcharacters } from './specialcharacters'
